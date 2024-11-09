c93af9a9f1ecc95cd4bed4f8e7989348e6fd6ab7
Ansh Rathod <anshrathod29@gmail.com>
2024-11-09 16:52:23 +0530

    what ?

---
+++ scripts/build_frei0r.sh
@@ -0,0 +1,6 @@
+export SOURCE="/Users/anshrathod/desktop/ffmpeg_build_play/sw"
+export COMPILED="/Users/anshrathod/desktop/ffmpeg_build_play/compile"
+
+echo '♻️  Start compiling aribb24'
+
+url = https://github.com/dyne/frei0r/archive/refs/tags/v2.3.3.tar.gz
